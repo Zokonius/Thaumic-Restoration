da44aa585dfee839ac796f0faabdac6355655ec3
Zoko061602 <32712367+Zoko061602@users.noreply.github.com>
2020-05-14 15:10:06 +0200

    version bump

--- src/main/java/com/Zoko061602/ThaumicRestoration/main/ThaumicRestoration.java
+++ src/main/java/com/Zoko061602/ThaumicRestoration/main/ThaumicRestoration.java
@@ -13,7 +13,7 @@ public class ThaumicRestoration {
 
     public static final String ModID = "thaumicrestoration";
     public static final String ModName = "Thaumic Restoration";
-    public static final String Version = "1.3.4";
+    public static final String Version = "1.4.0";
     public static final String Dependencies = "required-after:thaumcraft;"+
                                                 "after:jei;"+
                                                 "after:tconstruct;"+
